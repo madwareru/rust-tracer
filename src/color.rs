a3c567ee0957a93c1a33a6cb83e38e2f35a4b5fb
madwareru <madware.ru@gmail.com>
2020-06-25 12:09:13 +0300

    Made some changes so we now have color available to be cast from Vector3<f32>, changed main to use these changes

--- src/color.rs
+++ src/color.rs
@@ -1,3 +1,5 @@
+use cgmath::Vector3;
+
 #[derive(Clone, Copy)]
 pub struct Color{
     pub r: u8,
@@ -8,5 +10,15 @@ pub struct Color{
 impl Color {
     pub fn to_string(&self) -> String {
         format!("{}\t{}\t{}\t", self.r, self.g, self.b)
+    }
+}
+
+impl From<Vector3<f32>> for Color {
+    fn from(v: Vector3<f32>) -> Self {
+        Color {
+            r: v.x as u8,
+            g: v.y as u8,
+            b: v.z as u8
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
 mod color;
 mod picture;
 
-use color::Color;
 use picture::Picture;
+use cgmath::Vector3;
 
 fn main() {
     let mut pic = Picture::new(320, 200);
     pic.mutate(|colors, w, h| {
         for j in 0..h {
-            let j_float = j as f32 / h as f32 * 255.99;
             for i in 0..w {
-                let i_float = i as f32 / w as f32 * 255.99;
-                let clr = Color{r: i_float as u8, g: j_float as u8, b: 127};
-                colors[i + j * w] = clr;
+                let vec = Vector3::new(
+                    i as f32 / w as f32 * 255.99,
+                    j as f32 / h as f32 * 255.99,
+                    127.0
+                );
+                colors[i + j * w] = vec.into();
             }
         }
     });
